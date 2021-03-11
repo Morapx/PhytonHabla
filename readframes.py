4a0553e32759089723c8049237185937663d64d3
Morapx <bryan_alexis_1999@hotmail.com>
2021-03-10 22:35:07 -0700

    bytes a enteros

--- readframes.py
+++ readframes.py
@@ -1,11 +1,17 @@
 import wave
+import numpy as np
 
-#Cargar archivo wav en la variable
+#carcar archivo wav en la variable
+gm = wave.open('good-morningMan.wav', 'r')
 
-goodmorning = wave.open('good-morningMan.wav', 'r')
+#obtener todos los fromas del objeto wave
+frames = gm.readframes(-1)
 
-#Obtener todos los frames del objeto wave
-frames = goodmorning.readframes(-1)
+#mostrar el resultado de frames
+print(frames[:10])
 
-#mostrr el resultado de frames
-print(frames[:100])
+#Convierte el audio good morning de bytes a enteros
+ondaconvertida = np.frombuffer(frames, dtype='int16')
+
+#Imprime los 10 primeros valores de la onda.
+print(ondaconvertida[:10])
