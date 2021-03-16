11ba713151b920216a1addde20d54fbb62e93492
Morapx <bryan_alexis_1999@hotmail.com>
2021-03-15 23:01:02 -0700

    help

--- readframes.py
+++ readframes.py
@@ -1,17 +1,56 @@
 import wave
 import numpy as np
+import matplotlib.pyplot as plt
 
 #carcar archivo wav en la variable
-gm = wave.open('good-morningMan.wav', 'r')
+good_morning = wave.open('good-morningMan.wav', 'r')
 
 #obtener todos los fromas del objeto wave
-frames = gm.readframes(-1)
+frames = good_morning.readframes(-1)
 
 #mostrar el resultado de frames
-print(frames[:10])
+#print(signal_gm[:10])
+#print(signal_gm)
 
 #Convierte el audio good morning de bytes a enteros
 ondaconvertida = np.frombuffer(frames, dtype='int16')
 
 #Imprime los 10 primeros valores de la onda.
-print(ondaconvertida[:10])
+#print(ondaconvertida[:10])
+
+framerate_gm = good_morning.getframerate()
+
+print(framerate_gm)
+
+time_gm = np.linspace(start=0, stop=len(ondaconvertida)/framerate_gm, num=len(ondaconvertida))
+print(time_gm[:10])
+
+
+#lo mismo de arriba pero en good afternoon
+good_afternoon = wave.open('good-afternoon.wav', 'r')
+
+framesafter = good_afternoon.readframes(-1)
+
+ondaconvertidaafter = np.frombuffer(framesafter, dtype='int16')
+
+framerate_ga = good_afternoon.getframerate()
+
+print(framerate_ga)
+
+time_ga = np.linspace(start=0, stop=len(ondaconvertidaafter)/framerate_ga, num=len(ondaconvertidaafter))
+print(time_ga[:10])
+
+#Siguientes pasos
+#generacion de la grafica
+plt.title('good morning vs Good afternoon')
+
+#etiquetas de los ejes
+plt.xlabel('Tiempo (segundos)')
+plt.ylabel('Amplitud')
+
+#agregar informacion de las ondas para graficar
+plt.plot(time_gm, good_morning, label = 'Good morning')
+plt.plot(time_ga, good_afternoon, label= 'Good afternoon', alpha = 0.5)
+
+plt.legend()
+plt.show()
---
+++ readframeshomework.py
@@ -0,0 +1,58 @@
+import wave
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Lo mismo pero en cri-d-effroi-scream
+print('Scream')
+
+scream = wave.open('cri-d-effroi-scream.wav', 'r')
+
+framesscream = scream.readframes(-1)
+
+print(framesscream[:10])
+
+ondaconvertidascream = np.frombuffer(framesscream, dtype='int16')
+
+print(ondaconvertidascream[:10])
+
+framerate_scrm = scream.getframerate()
+
+print(framerate_scrm)
+
+time_scrm = np.linspace(start=0, stop=len(ondaconvertidascream)/framerate_scrm, num=len(ondaconvertidascream))
+print(time_scrm[:10])
+
+#Lo mismo pero en Kaa
+print('KaaAudio')
+screamkaa = wave.open('kaa.wav', 'r')
+
+frameskaa = screamkaa.readframes(-1)
+
+print(frameskaa[:10])
+
+ondaconvertidaskaa = np.frombuffer(frameskaa, dtype='int16')
+
+print(ondaconvertidaskaa[:10])
+
+framerate_kaa = screamkaa.getframerate()
+
+print(framerate_kaa)
+
+time_kaa = np.linspace(start=0, stop=len(ondaconvertidaskaa)/framerate_kaa, num=len(ondaconvertidaskaa))
+print(time_kaa[:10])
+
+
+#Siguientes pasos
+#generacion de la grafica
+plt.title('Scream vs Kaa')
+
+#etiquetas de los ejes
+plt.xlabel('Tiempo (segundos)')
+plt.ylabel('Amplitud')
+
+#agregar informacion de las ondas para graficar
+plt.plot(time_scrm, scream, label = 'scream')
+plt.plot(time_kaa, screamkaa, label= 'kaa', alpha = 0.5)
+
+plt.legend()
+plt.show()
